52bcb0909fc12e57c725849d2052521c7a2c1b53
radicines4 <r4dicines@gmail.com>
2025-04-09 11:47:22 +0200

    weird-program.cpp
    https://cses.fi/problemset/task/1068

---
+++ introductory-problems/weird-program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+// zadatak: https://cses.fi/problemset/task/1068
+int main()
+{
+    long long n; cin >> n;
+    while(true){
+        cout << n << " ";
+        if(n == 1) break;
+        if(n%2==0) n=n/2;
+        else n=n*3+1;
+    }
+    cout << "\n";
+    return 0;
+}
